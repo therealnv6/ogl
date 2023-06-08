248246dbff9ea152bdb5ea74711392da494a6a21
therealnv6 <devrawr@protonmail.com>
2023-06-08 11:51:08 +0200

    add rotating functions to raycast

--- include/voxel/ray.hpp
+++ include/voxel/ray.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <functional>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 #include <spdlog/spdlog.h>
 #include <vector>
 
@@ -38,6 +40,27 @@ namespace ray
 		void set_direction(glm::vec3 direction)
 		{
 			this->direction = direction;
+		}
+
+		void rotate_yaw(float angle_degrees)
+		{
+			float angle_rad = glm::radians(angle_degrees);
+
+			glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle_rad, glm::vec3(0.0f, 1.0f, 0.0f));
+
+			direction = glm::vec3(rotation * glm::vec4(direction, 0.0f));
+			direction = glm::normalize(direction);
+		}
+
+		void rotate_pitch(float angle_degrees)
+		{
+			float angle_rad = glm::radians(angle_degrees);
+
+			glm::vec3 right = glm::cross(direction, glm::vec3(0.0f, 1.0f, 0.0f));
+			glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle_rad, right);
+
+			direction = glm::vec3(rotation * glm::vec4(direction, 0.0f));
+			direction = glm::normalize(direction);
 		}
 
 	private:

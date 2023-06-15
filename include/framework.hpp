9726bbfb9817351e0573d732528e37cdcea61bd7
therealnv6 <devrawr@protonmail.com>
2023-06-15 18:38:39 +0200

    start working on seperating voxels

--- include/framework.hpp
+++ include/framework.hpp
@@ -3,9 +3,9 @@
 #include <window.hpp>
 
 #include "GLFW/glfw3.h"
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 #include <entt/entt.hpp>
 #include <spdlog/logger.h>
